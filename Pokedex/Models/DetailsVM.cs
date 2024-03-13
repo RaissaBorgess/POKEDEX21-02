905f285570b3710712c56e5da876b3faadbb0855
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-03-13 14:44:30 -0300

    142

---
+++ Pokedex/Models/DetailsVM.cs
@@ -0,0 +1,9 @@
+namespace Pokedex.Models;
+
+public class DeitailsVM
+{
+    public Pokemon Anterior { get; get; }
+    public Pokemon  Atual { get; get; }
+    public Pokemon Proximo { get; get; }
+    public Pokemon Tipos { get; get; }
+}
