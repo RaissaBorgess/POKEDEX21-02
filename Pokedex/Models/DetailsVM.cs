0b60f482b6918875e6dde28f51ee73f2d2d156a5
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-03-20 14:47:25 -0300

    fizemos ate a pag 157

--- Pokedex/Models/DetailsVM.cs
+++ Pokedex/Models/DetailsVM.cs
@@ -1,9 +1,9 @@
 namespace Pokedex.Models;
 
-public class DeitailsVM
+public class DetailsVM
 {
-    public Pokemon Anterior { get; get; }
-    public Pokemon  Atual { get; get; }
-    public Pokemon Proximo { get; get; }
-    public Pokemon Tipos { get; get; }
+    public Pokemon Anterior { get; set; }
+    public Pokemon  Atual { get; set; }
+    public Pokemon Proximo { get; set; }
+    public List<Tipo> Tipos { get; set; }
 }
